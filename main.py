787d6dfc9f5d61c33711f0a075f6189359b5a85d
mathric <mathric2015@gmail.com>
2024-03-13 21:57:07 -0500

    update argparse usage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import sys
@@ -39,30 +40,44 @@ def download_img(appid, overwrite=False):
 
 
 if __name__ == '__main__':
-    # write to json file
-    if len(sys.argv) > 1 and sys.argv[1] == 'init':
+    parser = argparse.ArgumentParser(
+        description='Help to download steam game images and load to eagle'
+    )
+    main_action_group = parser.add_argument_group('Main Action', 'Main Action')
+    exclusive_group = main_action_group.add_mutually_exclusive_group(required=True)
+    exclusive_group.add_argument('--init', action='store_true', help='init to get owned games json file')
+    exclusive_group.add_argument('--download_img', action='store_true', help='download images from owned games')
+    exclusive_group.add_argument('--eagle_load', action='store_true', help='load images to eagle')
+    args = parser.parse_args()
+
+    if args.init:
         with open('owned_games.json', 'w') as f:
             json.dump(get_owned_games(), f)
+    elif args.download_img:
+        if not os.path.exists('img'):
+            os.makedirs('img')
 
-    if not os.path.exists('img'):
-        os.makedirs('img')
-
-    # load from json file
-    failed_download_appid = []
-    with open('owned_games.json', 'r') as f:
-        data = json.load(f)
-        for i in tqdm(range(len(data['response']['games']))):
-            game = data['response']['games'][i]
-            try:
-                download_img(game['appid'])
-            except Exception as e:
-                failed_download_appid.append(
-                    {
-                        'appid': game['appid'],
-                        'name': game['name']
-                    }
-                )
-    
-    # write failed img download games to file
-    with open('failed_img_download.json', 'w') as f:
-        json.dump(failed_download_appid, f)
+        # load from json file
+        failed_download_appid = []
+        with open('owned_games.json', 'r') as f:
+            data = json.load(f)
+            for i in tqdm(range(len(data['response']['games']))):
+                game = data['response']['games'][i]
+                try:
+                    download_img(game['appid'])
+                except Exception as e:
+                    failed_download_appid.append(
+                        {
+                            'appid': game['appid'],
+                            'name': game['name']
+                        }
+                    )
+        
+        # write failed img download games to file
+        with open('failed_img_download.json', 'w') as f:
+            json.dump(failed_download_appid, f)
+    elif args.eagle_load:
+        print('eagle_load')
+    else:
+        parser.print_help()
+        sys.exit(1)
